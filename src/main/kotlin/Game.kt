52c193d15dfd64a134e8040460bd1c8f6059dcec
jeniebeez <richardloyj@gmail.com>
2021-06-28 14:28:05 +0800

    c13-1/2

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,31 @@
+fun main() {
+    val player = Player("Madrigal",89,true,false)
+    player.castFireball()
+    //Aura
+    val auraColor = player.auraColor
+    //com.bignerdranch.nyethack.Player Status
+    printPlayerStatus(player)
+    //Drunk Status
+    val numFireballs =(2..10).random()
+    printDrunkStatus(numFireballs)
+}
+
+private fun printPlayerStatus(player: Player) {
+    println("光環顏色：${player.auraColor}" + "    走運嗎？${if (player.isBlessed) "是的" else "很背"}")
+    println("${player.name} ${player.healthStatus}")
+}
+
+private fun printDrunkStatus(numFireballs: Int) {
+    println("酒醉程度：${drunkStatus(numFireballs)}")
+}
+
+private fun drunkStatus(numFireballs:Int,
+                        drunkness:Int=numFireballs* 5)=
+    when (drunkness) {
+        in 1..10 -> "微醺"
+        in 11..20 -> "微醉"
+        in 21..30 -> "醉了"
+        in 31..40 -> "大醉"
+        in 31..50 -> "爛醉如泥"
+        else -> null
+    }
---
+++ src/main/kotlin/Player.kt
@@ -0,0 +1,43 @@
+class Player(_name:String,_healthPoints:Int,_isBlessed:Boolean,_isImmortal:Boolean){
+    var name = _name
+        get() = field.capitalize()
+        private set(value) {
+            field = value.trim() //trim刪除傳入值的前後空格
+        }
+    var healthPoints = _healthPoints
+    var isBlessed = _isBlessed
+    private val isImmortal = _isImmortal
+    val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0) *20).toInt()//返回（random的（110-生命值/100）為幾次方(指數)的值）
+    //Aura
+    val auraColor = auraColor(karma)
+    //Health Status
+    val healthStatus = formatHealthStatus()
+
+    fun castFireball(numFireballs:Int =2)=
+        println("一杯Fireball酒應運而生。(x$numFireballs)")
+
+    fun formatHealthStatus()=
+        when (healthPoints) {
+            100 -> "健康狀態極佳"
+            in 90..99 -> "有一些小擦傷"
+            in 75..89 -> if (isBlessed) {
+                "雖有一些傷口，但恢復很快"
+            } else {
+                "有一些傷口"
+            }
+            in 15..74 -> "嚴重受傷"
+            //顯示玩家狀態
+            else -> "情況不妙"
+        }
+
+    fun auraColor(
+        karma: Int
+    ): String =
+        when (karma) {
+            in (0..5) -> "紅色"
+            in (6..10) -> "橘色"
+            in (11..15) -> "紫色"
+            in (16..20) -> "綠色"
+            else -> "無光環"
+        }
+}
